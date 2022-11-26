713ca3d98f657529999cbfb08a866f15f6120200
envyvox <kkxo.mail@gmail.com>
2022-11-26 14:23:04 +0200

    Fix generate weather not executing daily

--- Disfarm.Services/Discord/Client/Events/OnReady.cs
+++ Disfarm.Services/Discord/Client/Events/OnReady.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using Discord.Interactions;
 using Disfarm.Services.Discord.Emote.Commands;
 using Disfarm.Services.Discord.Extensions;
+using Disfarm.Services.Hangfire.BackgroundJobs.GenerateWeather;
 using Disfarm.Services.Hangfire.BackgroundJobs.ResetDailyRewards;
 using Hangfire;
 using MediatR;
@@ -13,71 +14,75 @@ using Microsoft.Extensions.Options;
 
 namespace Disfarm.Services.Discord.Client.Events
 {
-	public record OnReady(InteractionService InteractionService) : IRequest;
+    public record OnReady(InteractionService InteractionService) : IRequest;
 
-	public class OnReadyHandler : IRequestHandler<OnReady>
-	{
-		private readonly ILogger<OnReadyHandler> _logger;
-		private readonly IMediator _mediator;
-		private readonly IHostApplicationLifetime _lifetime;
-		private readonly IHostEnvironment _environment;
-		private readonly TimeZoneInfo _timeZoneInfo;
-		private readonly DiscordClientOptions _options;
+    public class OnReadyHandler : IRequestHandler<OnReady>
+    {
+        private readonly ILogger<OnReadyHandler> _logger;
+        private readonly IMediator _mediator;
+        private readonly IHostApplicationLifetime _lifetime;
+        private readonly IHostEnvironment _environment;
+        private readonly TimeZoneInfo _timeZoneInfo;
+        private readonly DiscordClientOptions _options;
 
-		public OnReadyHandler(
-			IOptions<DiscordClientOptions> options,
-			ILogger<OnReadyHandler> logger,
-			IMediator mediator,
-			IHostApplicationLifetime lifetime,
-			IHostEnvironment environment,
-			TimeZoneInfo timeZoneInfo)
-		{
-			_logger = logger;
-			_mediator = mediator;
-			_lifetime = lifetime;
-			_environment = environment;
-			_timeZoneInfo = timeZoneInfo;
-			_options = options.Value;
-		}
+        public OnReadyHandler(
+            IOptions<DiscordClientOptions> options,
+            ILogger<OnReadyHandler> logger,
+            IMediator mediator,
+            IHostApplicationLifetime lifetime,
+            IHostEnvironment environment,
+            TimeZoneInfo timeZoneInfo)
+        {
+            _logger = logger;
+            _mediator = mediator;
+            _lifetime = lifetime;
+            _environment = environment;
+            _timeZoneInfo = timeZoneInfo;
+            _options = options.Value;
+        }
 
-		public async Task<Unit> Handle(OnReady request, CancellationToken cancellationToken)
-		{
-			try
-			{
-				await _mediator.Send(new SyncEmotesCommand());
+        public async Task<Unit> Handle(OnReady request, CancellationToken cancellationToken)
+        {
+            try
+            {
+                await _mediator.Send(new SyncEmotesCommand());
 
-				RecurringJob.AddOrUpdate<IResetDailyRewardJob>("reset-daily-reward",
-					x => x.Execute(),
-					Cron.Weekly, _timeZoneInfo);
+                RecurringJob.AddOrUpdate<IResetDailyRewardJob>("reset-daily-reward",
+                    x => x.Execute(),
+                    Cron.Weekly, _timeZoneInfo);
 
-				if (_environment.IsDevelopment())
-				{
-					_logger.LogInformation(
-						"Environment is development. Registering commands to guild {GuildId}",
-						_options.BetaGuildId);
+                RecurringJob.AddOrUpdate<IGenerateWeatherJob>("generate-weather",
+                    x => x.Execute(),
+                    Cron.Daily, _timeZoneInfo);
 
-					await request.InteractionService.RegisterCommandsToGuildAsync(_options.BetaGuildId);
-				}
-				else
-				{
-					_logger.LogInformation(
-						"Environment is production. Registering commands globally");
+                if (_environment.IsDevelopment())
+                {
+                    _logger.LogInformation(
+                        "Environment is development. Registering commands to guild {GuildId}",
+                        _options.BetaGuildId);
 
-					await request.InteractionService.RegisterCommandsGloballyAsync();
-				}
+                    await request.InteractionService.RegisterCommandsToGuildAsync(_options.BetaGuildId);
+                }
+                else
+                {
+                    _logger.LogInformation(
+                        "Environment is production. Registering commands globally");
 
-				_logger.LogInformation(
-					"Bot started");
-			}
-			catch (Exception e)
-			{
-				_logger.LogCritical(e,
-					"Unable to startup the bot. Application will now exit");
+                    await request.InteractionService.RegisterCommandsGloballyAsync();
+                }
 
-				_lifetime.StopApplication();
-			}
+                _logger.LogInformation(
+                    "Bot started");
+            }
+            catch (Exception e)
+            {
+                _logger.LogCritical(e,
+                    "Unable to startup the bot. Application will now exit");
 
-			return new Unit();
-		}
-	}
+                _lifetime.StopApplication();
+            }
+
+            return new Unit();
+        }
+    }
 }
